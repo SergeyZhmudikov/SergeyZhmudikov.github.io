552ceb4454bb22d015b1d1616e3aa2165e54a90a
SergeyZhmudikov <energys17@gmail.com>
2017-11-03 16:46:02 +0300

    Task8. Configuration

---
+++ FEwebproject/js/app.js
@@ -0,0 +1,3 @@
+import example from './subapp';
+example("Sergey");
+import style from '../css/style.css';
---
+++ FEwebproject/js/subapp.js
@@ -0,0 +1,3 @@
+export default function example(message) {
+    alert(`Welcome ${message}`);
+}
